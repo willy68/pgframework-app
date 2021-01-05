fcdadbaba0330cd0d2316b603d5e14cbf55df38c
William Lety <william.lety@gmail.com>
2021-01-05 18:35:36 +0100

    Test if category exist

--- app/Blog/Actions/CategoryShowAction.php
+++ app/Blog/Actions/CategoryShowAction.php
@@ -2,11 +2,12 @@
 
 namespace App\Blog\Actions;
 
-use App\Blog\Models\Categories;
+use Framework\Router;
 use App\Blog\Models\Posts;
+use GuzzleHttp\Psr7\Response;
+use App\Blog\Models\Categories;
 use Framework\Actions\RouterAwareAction;
 use Framework\Renderer\RendererInterface;
-use Framework\Router;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 /**
@@ -55,13 +56,21 @@ class CategoryShowAction
     public function __invoke(Request $request)
     {
         $category = Categories::find_by_slug($request->getAttribute('slug'));
-        $params = $request->getQueryParams();
-        // Init Query
-        $posts = Posts::setPaginatedQuery(Posts::findPublicForCategory($category->id))
+        if ($category) {
+            $params = $request->getQueryParams();
+            // Init Query
+            $posts = Posts::setPaginatedQuery(Posts::findPublicForCategory($category->id))
                 ::paginate(12, $params['p'] ?? 1);
-        $categories = Categories::find('all');
-        $page = $params['p'] ?? 1;
+            $categories = Categories::find('all');
+            $page = $params['p'] ?? 1;
 
-        return $this->renderer->render('@blog/index', compact('posts', 'categories', 'category', 'page'));
+            return $this->renderer->render('@blog/index', compact('posts', 'categories', 'category', 'page'));
+        }
+        else {
+            return new Response(404, [], $this->renderer->render(
+                'error404',
+                ['message' => 'Impossible de trouver cette categorie: ' . $request->getAttribute('slug')])
+            );
+        }
     }
 }
