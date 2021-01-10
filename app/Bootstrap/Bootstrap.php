e2b451ec4bf402ae01e1e8657e0940af82c05713
willy68 <william.lety@gmail.com>
2021-01-10 15:56:41 +0100

    Test existence $basePath

--- app/Bootstrap/Bootstrap.php
+++ app/Bootstrap/Bootstrap.php
@@ -7,6 +7,11 @@ use Symfony\Component\Dotenv\Dotenv;
 if (!class_exists(Dotenv::class)) {
     throw new Exception("le library symfony/dotenv est pas installée, lancez composer symfony/dotenv!");
 }
+
+if (isset($basePath)) {
+    $basePath = dirname(dirname(__DIR__));
+}
+
 $dotenv = new Dotenv();
 $dotenv->loadEnv($basePath . '/.env');
 
